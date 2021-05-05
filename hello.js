f99a51250ec2d97290c669fca2a9d1dab0bb5511
Luis Contreras Issa <tremenditomen@gmail.com>
2021-05-05 09:35:19 -0600

    function hello

--- hello.js
+++ hello.js
@@ -1,8 +1,13 @@
-const sayHello = function (name){
+/*const sayHello = function (name){
   console.log("Hello " + name);
 return
 
 }
 sayHello('jeff');
 sayHello('El presidente')
-sayHello('Eduardo')
+sayHello('Eduardo')*/
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
