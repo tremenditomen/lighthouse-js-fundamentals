2ea80cb8e7891c41abd542b5e470254edf3e25ef
Luis Contreras Issa <tremenditomen@gmail.com>
2021-05-05 15:31:25 -0600

    this function calculates age and passes a name through

---
+++ agecalc.js
@@ -0,0 +1,6 @@
+let ageCalculator = (name,born , currentYear) => {
+  return name + " is " + (currentYear - born) + " years old.";
+};
+
+console.log(ageCalculator("Suzie", 1984, 2016));
+console.log(ageCalculator("Jack", 2004, 2016));
