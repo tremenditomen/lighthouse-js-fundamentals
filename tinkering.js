ac933c98a2baf1967f845cfef8f19774414b000f
Luis Contreras Issa <tremenditomen@gmail.com>
2021-05-05 08:27:43 -0600

    i had two files that needed to be aded

--- tinkering.js
+++ tinkering.js
@@ -1,12 +1,10 @@
-const raining = true;
-const cold = false;
-
-if (raining) {
-  console.log("Don't forget your umbrella!");
-}
-
-if (cold) {
-  console.log("Make sure you pick out a scarf!");
-}
-
-console.log("Now you're ready to go outside!");
+const whichSchool = function (age) {
+  if (age >= 1 && age <= 12) {
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else if (age >= 19 && age <= 100) {
+    return "Lighthouse Labs";
+  }
+};
+console.log(whichSchool(15));
