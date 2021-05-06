f341da35c00b301b871b0551f0c25a32a1616fb8
Luis Contreras Issa <tremenditomen@gmail.com>
2021-05-06 10:11:05 -0600

    playing with arrays

---
+++ ice-cream.js
@@ -0,0 +1,13 @@
+const iceCreamFlavourse = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry"
+];
+console.log(iceCreamFlavourse);
+iceCreamFlavourse.push("root beer");
+console.log(iceCreamFlavourse)
+console.log(iceCreamFlavourse[0]);
+console.log(iceCreamFlavourse[5]);
+console.log(iceCreamFlavourse.length);
