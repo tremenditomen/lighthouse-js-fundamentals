4b0bb09261a92020b43101829aa14ca528fd3295
Luis Contreras Issa <tremenditomen@gmail.com>
2021-05-08 12:52:19 -0600

    passing cars challange

---
+++ carPassing.js
@@ -0,0 +1,25 @@
+const cars = [
+  {
+    time: 1568329654807,
+    speed: 40
+  },
+  {
+    time: 1568329821632,
+    speed: 42
+  },
+  {
+    time: 1568331115463,
+    speed: 35
+  }
+];
+
+const speed = 38;
+const carPassing = (cars, speed) => {
+  let newObject = {
+    time: Date.now(),
+    speed: speed
+  };
+  cars.push(newObject);
+  return cars;
+};
+console.log(carPassing(cars, speed));
